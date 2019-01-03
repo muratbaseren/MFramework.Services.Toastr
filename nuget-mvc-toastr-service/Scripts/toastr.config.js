03c3e84c33f34794a9a5263ef4384a384665d54c
Murat Başeren <muratbaseren@hotmail.com>
2019-01-03 14:17:52 +0300

    Added nuget package, js file and updated nuspec

---
+++ nuget-mvc-toastr-service/Scripts/toastr.config.js
@@ -0,0 +1,12 @@
+﻿toastr.options.preventDuplicates = false;
+toastr.options.closeButton = true;
+//toastr.options.escapeHtml = true;
+
+//Prevent from Auto Hiding
+//toastr.options.timeOut = 0;
+//toastr.options.extendedTimeOut = 0;
+
+//toastr.options.onShown = function () { console.log('hello'); }
+//toastr.options.onHidden = function () { console.log('goodbye'); }
+//toastr.options.onclick = function () { console.log('clicked'); }
+//toastr.options.onCloseClick = function () { console.log('close button clicked'); }
