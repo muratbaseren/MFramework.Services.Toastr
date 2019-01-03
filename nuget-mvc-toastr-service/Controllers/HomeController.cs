f2e0f9a489312dfe2de4a4c7441d0de4f1d36d5f
Murat Başeren <muratbaseren@hotmail.com>
2019-01-03 14:24:47 +0300

    Added sample code

---
+++ nuget-mvc-toastr-service/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace nuget_mvc_toastr_service.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            ToastrService.AddToUserQueue(message: "Hello world.", title: "Hello W!", type: ToastrType.Warning);
+
+            return View();
+        }
+    }
+}
